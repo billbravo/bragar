1da2215d7b41ff9d2f700ef6a470079e6f98a113
Jose Luis Bravo <billbravo@gmail.com>
2025-08-18 22:44:09 -0500

    Se actualizan las pruebas del backend

--- backend_tests.py
+++ backend_tests.py
@@ -1,12 +1,29 @@
-from backend.cliente import listar_clientes
-from backend.cliente import consultar_cliente
+from backend.producto import listar_productos
+from backend.producto import consultar_producto
+from backend.producto import crear_producto
+from backend.producto import eliminar_producto
+from backend.producto import actualizar_producto
 
+print("Listar productos...")
+productos = listar_productos()
+print(productos)
 
-print("Listar clientes...")
-clientes = listar_clientes()
-print(clientes)
+print("Crear producto...")
+producto_creado = crear_producto("9999", "iPhone 16", 600, 10)
+print(f"Producto creado: {producto_creado}")
 
+print("Consultar producto...")
+producto = consultar_producto("9999")
+print(producto)
 
-print("Consultar clientes...")
-cliente = consultar_cliente("1101")
-print(cliente)
+print("Actualizar producto...")
+producto_actualizado = actualizar_producto("9999", "iPhone 16 Pro", 900, 5)
+print(f"Producto actualizado: {producto_actualizado}")
+producto = consultar_producto("9999")
+print(producto)
+
+print("Eliminar producto...")
+producto_eliminado = eliminar_producto("9999")
+print(f"Producto eliminado: {producto_eliminado}")
+productos = listar_productos()
+print(productos)
